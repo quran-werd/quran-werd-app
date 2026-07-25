a76830a3ee83093013fb9e70585bec02d8ea8c0a
osamaths <osama.alshbikat@gmail.com>
2026-07-25 23:55:19 +0300

    chore: migrate to expo with eas build

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        allowUndefined: true,
-      },
-    ],
-  ],
+  presets: ['babel-preset-expo'],
 };
